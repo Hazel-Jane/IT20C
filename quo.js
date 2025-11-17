63aefea8582ae731a664902737b98834e0f63443
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-17 21:50:30 +0800

    Added if else

--- quo.js
+++
@@ -1,5 +0,0 @@
-function quo (num1, num2) {
-    quo = num1/num2;
-    console.log(quo);
-}
-quo(2,3);
--- sum.js
+++
@@ -1,6 +0,0 @@
-function sum (num1, num2) {
-    sumf = num1-num2;
-    console.log(sum);
-}
-sum(2,3);
-
