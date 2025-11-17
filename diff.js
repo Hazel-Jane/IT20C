f02b3e2362b8afec861994eacf85a58c0b1f4f90
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-17 21:23:12 +0800

    Initial commit

---
+++ diff.js
@@ -0,0 +1,5 @@
+function diff (num1, num2) {
+    diff = num1-num2;
+    console.log(diff);
+}
+diff(2,3);
---
+++ quo.js
@@ -0,0 +1,5 @@
+function quo (num1, num2) {
+    quo = num1/num2;
+    console.log(quo);
+}
+quo(2,3);
