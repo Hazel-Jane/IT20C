6daaba1cd140df1e0bc6e134b8d8968aa55c5945
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-20 08:32:00 +0800

    Added while.loop.js

---
+++ while-loop.js
@@ -0,0 +1,33 @@
+// console.log("Descending");
+// i=5;
+
+// while (i <= 1){
+//           console.log(i);
+//           i--;
+// }
+// console.log("Ascending")
+// i=1;
+
+// while (x <= 5){
+//           console.log(x);
+//           x++;
+// }
+
+// output = " ";
+// x=1;
+
+// while (x<=5) {
+//           oputput += x + " ";
+//           x++;
+// }
+// console.log(output);
+
+arr=[1,2,3];
+output = " ";
+index = 0;
+
+while(index <arr.length){
+          output += arr[index] + " ";
+          index++;
+}
+console.log(output);
