a65a73b2b95ce15ea5ed5a842d0a5704562798a0
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-23 22:45:24 +0800

    Added else if

--- cs.js
+++ cs.js
@@ -1,28 +1,28 @@
-function legalAge(age) {
-    if (age >= 18) {
-        console.log("You are an adult");
-    } else {
-        console.log("pananghid saimong parents");
-    }
-}
-legalAge(50);
-
-// function computerGrade(grade) {
-//     if (grade >= 94.5) {
-//         console.log("1");
-//     } else if (grade >= 91.5) {
-//         console.log("1.25");
-//     } else if (grade >= 89.5) {
-//         console.log("1.50");
-//     } else if (grade >= 86.5) {
-//         console.log("1.75");
-//     } else if (grade >= 84.5) {
-//         console.log("2");
+// function legalAge(age) {
+//     if (age >= 18) {
+//         console.log("You are an adult");
 //     } else {
-//         console.log("Not within the transmutation");
+//         console.log("pananghid saimong parents");
 //     }
 // }
-// computerGrade(85);
+// legalAge(50);
+
+function computerGrade(grade) {
+    if (grade >= 94.5) {
+        console.log("1");
+    } else if (grade >= 91.5) {
+        console.log("1.25");
+    } else if (grade >= 89.5) {
+        console.log("1.50");
+    } else if (grade >= 86.5) {
+        console.log("1.75");
+    } else if (grade >= 84.5) {
+        console.log("2");
+    } else {
+        console.log("Not within the transmutation");
+    }
+}
+computerGrade(85);
 
 // function login(username, password) {
 //     if (username === "Lezah") {
