d8e26b0151473e2d0d3ff56c3a631b384fa1c9e1
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-23 22:38:06 +0800

    Added quotient

--- math.js
+++ math.js
@@ -10,14 +10,14 @@
 // }
 // diff(2,3);
 
-function prod (num1, num2) {
-    prod = num1*num2;
-    console.log(prod);
-}
-prod(2,3);
-
-// function quo (num1, num2) {
-//     quo = num1/num2;
-//     console.log(quo);
+// function prod (num1, num2) {
+//     prod = num1*num2;
+//     console.log(prod);
 // }
-// quo(2,3);
+// prod(2,3);
+
+function quo (num1, num2) {
+    quo = num1/num2;
+    console.log(quo);
+}
+quo(2,3);
