46162fe5c5fb47434b7fd321cb29dcff42a15b3b
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-23 22:37:29 +0800

    Added multiplication

--- math.js
+++ math.js
@@ -4,17 +4,17 @@
 // }
 // sum(2,3);
 
-function diff (num1, num2) {
-    diff = num1-num2;
-    console.log(diff);
-}
-diff(2,3);
-
-// function prod (num1, num2) {
-//     prod = num1*num2;
-//     console.log(prod);
+// function diff (num1, num2) {
+//     diff = num1-num2;
+//     console.log(diff);
 // }
-// prod(2,3);
+// diff(2,3);
+
+function prod (num1, num2) {
+    prod = num1*num2;
+    console.log(prod);
+}
+prod(2,3);
 
 // function quo (num1, num2) {
 //     quo = num1/num2;
