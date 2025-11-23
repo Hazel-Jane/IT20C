093ca1d91825fba4872284bfed8ef4149aabca96
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-23 22:36:44 +0800

    Added Subtraction

--- math.js
+++ math.js
@@ -1,14 +1,14 @@
-function sum (num1, num2) {
-    sumf = num1+num2;
-    console.log(sum);
-}
-sum(2,3);
-
-// function diff (num1, num2) {
-//     diff = num1-num2;
-//     console.log(diff);
+// function sum (num1, num2) {
+//     sumf = num1+num2;
+//     console.log(sum);
 // }
-// diff(2,3);
+// sum(2,3);
+
+function diff (num1, num2) {
+    diff = num1-num2;
+    console.log(diff);
+}
+diff(2,3);
 
 // function prod (num1, num2) {
 //     prod = num1*num2;
