6190bb21235fac695b08e7f6eb51f7b762e7bca7
Hazel Jane Lihotan <hjlihotan@gmail.com>
2025-11-24 08:34:11 +0800

    added constructor

---
+++ DSA/array.js
@@ -0,0 +1,85 @@
+class Array {
+          constructor(initialData =[]){
+                    this.data = {}
+                    this.length = 0;
+
+                    for(let i=0; i < initialData.length; i++){
+                         this.data[i] = initialData[i];
+                    this.length++;       
+                    }
+          }
+
+
+  traverse(){
+         let output = " ";
+
+              for(let i=0; i<this.length; i++){
+                    output += this.data[i] + " ";
+     }
+          console.log(output);
+         }
+
+// insert(index,data){
+ 
+//            //correcting statement
+//            if(index < 0 || index > this.length){
+//                     console.log("Invalid Index");
+//                     return;
+//            }
+
+//            // shifting condition
+
+//            for(let i = this.length;i > index; i--){
+//                     this.data[i] = this.data[i-1]
+//            }
+
+//            this.data[index] = element;
+//            this.length++;
+// }
+
+// search(element){
+//       for(let i=0; i < this.length; i++){
+//           if(this.data[i] === element){
+//                     console.log(elemen + "is found at index");
+//           }
+//       }
+//      console.log(element + "is not found at any index");
+//      return -1;
+// }
+
+// update(index,element){
+//           // correcting statement
+//           if(index < 0 || index >= this.length){
+//                     conbsole.log("Invalid index");
+//           }
+//           this.data[index] = element;
+// }
+
+// delete(index){
+// // correcting statement
+//           if(index < 0 || index >= this.length){
+//                     console.log("Invalid Index");
+//                     return;
+//           }
+//          // shifting 
+//            for(let i = this.length;i > index; i++){
+//                     this.data[i] = this.data[i-1]
+//            }
+
+//           delete this.data[this.length-1]
+//            this.length--;
+// }
+}
+
+
+
+//
+
+arr1= new Array([4,2,3]);
+
+arr1.traverse();
+arr1.insert(1,50);
+arr1.traverse();
+// arr1.delete(0);
+arr1.traverse();
+
